dec63e19533e6a38572afcfd38eac472342b3ec5
Dwight Marie <dwightmarie@gmail.com>
2019-09-26 03:45:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,10 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+array = []
+
+  array.each do |value_to_find|
+  puts "The current array item is: #{item}"
+end
+  
 end
 
-def find_max_value(array)
-  # Add your solution here
-end
-
-def find_min_value(array)
-  # Add your solution here
-end
+find_element_index(scale,2)
