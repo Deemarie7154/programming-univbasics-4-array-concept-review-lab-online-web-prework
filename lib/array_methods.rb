718b4faacac9e9e86b90cd6cc1cdbf24e28241f9
Dwight Marie <dwightmarie@gmail.com>
2019-09-26 05:31:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,12 +8,13 @@ array = []
 end
 end
 
-def find_max_value(array)
+
+def find_min_value(array)
 
  counter = 0
  
   while array[counter] do
-   return p array.sort.last
+   return p array.sort.first
   break
   end
 end
