c823384b087d5f685f420e27fcddef11a61b3080
Dwight Marie <dwightmarie@gmail.com>
2019-09-26 04:10:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
-storage = []
+array = []
 
   i = 0   
   while i < array.length 
-  storage = 
+   = 
   puts storage.index(value_to_find)
 end
   return storage
